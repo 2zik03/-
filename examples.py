5bb3d8868a85b64d9965c170f71cbb9224df103c
Алексей <168646186+2zik03@users.noreply.github.com>
2024-05-01 16:17:08 +0300

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,12 @@
+a, b = 10, 5
+if a > b:
+    print("a > b")
+
+if a > b and a > 0:
+    print("успех")
+
+if (a > b) and (a > 0 or b < 100):
+    print('успех')
+
+if 5 < b and b < 10:
+    print("успех")
